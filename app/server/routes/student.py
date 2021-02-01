2446a272ee771dbbde18c7011a5112dea9c9a53e
cgarzon <gagocaan@gmail.com>
2021-02-01 00:10:36 -0500

    Se agrega la actualizacion y eliminacion de objetos

--- app/server/routes/student.py
+++ app/server/routes/student.py
@@ -38,3 +38,29 @@ async def get_student_data(id):
     if student:
         return ResponseModel(student, "Student data retrieved successfully")
     return ErrorResponseModel("An error occurred.", 404, "Student doesn't exist.")
+
+
+@router.put("/{id}")
+async def update_student_data(id: str, req: UpdateStudentModel = Body(...)):
+    req = {k: v for k, v in req.dict().items() if v is not None}
+    updated_student = await update_student(id, req)
+    if updated_student:
+        return ResponseModel(
+            "Student with ID: {} name update is successful".format(id),
+            "Student name updated successfully",
+        )
+    return ErrorResponseModel(
+        "An error occurred", 404, "There was an error updating the student data.",
+    )
+
+
+@router.delete("/{id}", response_description="Student data deleted from the database")
+async def delete_student_data(id: str):
+    deleted_student = await delete_student(id)
+    if deleted_student:
+        return ResponseModel(
+            "Student with ID: {} removed".format(id), "Student deleted successfully"
+        )
+    return ErrorResponseModel(
+        "An error occurred", 404, "Student with id {0} doesn't exist".format(id)
+    )
